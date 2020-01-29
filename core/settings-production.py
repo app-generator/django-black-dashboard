1f24becb45b3c739c50d7b04d31fc74bc788873a
Adi Chirilov <developer@rosoftware.ro>
2020-01-29 21:47:47 +0200

    Dummy

--- core/settings-production.py
+++ core/settings-production.py
@@ -112,7 +112,6 @@ USE_TZ = True
 
 # STATIC_ROOT production load 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
 
 # STATIC_ROOT development load 
 STATICFILES_DIRS = (
