ffcc52639417721eafffd25d956930c3d8a60200
Adi Chirilov <developer@rosoftware.ro>
2020-01-29 22:14:51 +0200

    Dummy

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'localhost' , '127.0.0.1', config('PROD_HOST') ]
 
 # Application definition
 
