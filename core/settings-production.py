802814ef6fed73d4bb87f74037adab6ffc5f6078
Adi Chirilov <developer@rosoftware.ro>
2020-02-01 14:08:41 +0200

    Heroku Deploy Tests

--- core/settings-production.py
+++ core/settings-production.py
@@ -8,6 +8,7 @@ import os
 from decouple import config
 from unipath import Path
 import django_heroku
+import dj_database_url 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR    = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -35,6 +36,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -109,14 +111,14 @@ USE_L10N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
-# STATIC_ROOT development load 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "core/static"),
-)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
--- core/wsgi.py
+++ core/wsgi.py
@@ -5,9 +5,10 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 import os
-
+from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings-production')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
