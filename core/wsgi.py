0e6324a30447a608c7d08b5236ce184d6354313b
Adi Chirilov <developer@rosoftware.ro>
2020-02-10 08:39:20 +0200

    Dummy :)

--- core/wsgi.py
+++ core/wsgi.py
@@ -7,6 +7,6 @@ Copyright (c) 2019 - present AppSeed.us
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings-production')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
