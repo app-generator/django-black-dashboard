4d3d965dd9930d14ceb3756c138f241139790b26
App Generator <51070104+app-generator@users.noreply.github.com>
2024-04-16 19:40:43 +0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -52,7 +52,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django-browser-reload",
+    "django_browser_reload",
 
     "home",
 ]
